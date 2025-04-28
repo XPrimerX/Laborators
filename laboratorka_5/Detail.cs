5ea40eca94f8a837fd2148e7ff42f8af46b6d13b
XPrimerX <spakantonpetr@gmail.com>
2025-04-28 09:49:38 +0300

    Переробив лабораторну роботу 5

---
+++ laboratorka_5/Detail.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+
+
+namespace laboratorka_5
+{
+    enum TransportType
+    {
+        Ground,
+        Air,
+        Water
+    }
+    class Detail
+    {
+        public Person Manufactur;
+        public string opis_detail;
+        public double Weight;
+        //Конструктор з трьома параметрами
+        public Detail(Person manufactur, string opis_detail, double weight)
+        {
+            Manufactur = manufactur;
+            this.opis_detail = opis_detail;
+            Weight = weight;
+        }
+        //Конструктор без параметрів
+        public Detail()
+        {
+            Manufactur = new Person();
+            opis_detail = "Опис";
+            Weight = 20;
+        }
+        public Person ManuFactur
+        {
+            get { return Manufactur; }
+            set { if (value != null) Manufactur = value;
+                else throw new ArgumentOutOfRangeException();
+            }
+        }
+        public string Opis
+        {
+            get { return opis_detail; }
+            set
+            {
+                if (value.Length > 0) opis_detail = value;
+                else throw new ArgumentOutOfRangeException();
+            }
+        }
+        public double Weight_detail
+        {
+            get { return Weight; }
+            set { if (value > 0) Weight = value;
+                else throw new ArgumentOutOfRangeException();
+
+            }
+        
+        }
+        public override string ToString()
+        {
+            return "Виробник деталі: " + Manufactur + "\nОпис деталі: " + opis_detail + "\nВага деталі: " + Weight;
+        }
+
+
+
+    }
+}
