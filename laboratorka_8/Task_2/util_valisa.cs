afa49e10dd3c98b16f8abc6ea99e5b954e4e84a5
XPrimerX <spakantonpetr@gmail.com>
2025-05-05 14:36:47 +0300

    додав лабораторку 8

---
+++ laboratorka_8/Task_2/util_valisa.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laboratorka_8.Task_2
+{
+    class util_valisa
+    {
+        public static Random rnd = new Random();
+        private static string[] Color =
+        {
+            "Жовтий","Синій","Червоний"
+        };
+        private static string[] Firma =
+        {
+            "Гучі","Луївітон","Прада"
+        };
+        private static string retColor()
+        {
+           return Color[rnd.Next(Color.Length)];
+        }
+        private static string retFirma()
+        {
+            return Firma[rnd.Next(Firma.Length)];
+        }
+        private static double retWeight()
+        {
+            return rnd.Next(3, 10); 
+        }
+        private static double retObem()
+        {
+            return rnd.Next(20, 80);
+        }
+        public static Valisa genereteValisa()
+        {
+            return new Valisa(retColor(), retFirma(), retWeight(), retObem());
+        }
+    }
+}
