fe23ac55ca751086cecc4dcff49ddff87e049009
XPrimerX <spakantonpetr@gmail.com>
2025-09-24 20:45:55 +0300

    Додав лабораторні

---
+++ Лабораторні курс 1/Laboratorka_3/Program.cs
@@ -0,0 +1,505 @@
+﻿
+namespace Laboratorka_3
+{
+    internal class Program
+    {
+        static Random rnd = new Random();
+        static (int num_otr, long sum_otr) Videm_element(int[] iArray)
+        {
+            long sum_otr = 0;//сума від'ємних елементів
+            int num_otr = 0; //кількість від'ємних елементів
+            foreach (int num in iArray)
+            {
+                if (num < 0)
+                {
+                    sum_otr += num;
+                    num_otr++;
+                }
+            }
+            return (num_otr, sum_otr);
+        }
+      
+        public static void Change_five(int[] iArray)
+        {
+            for (int i = 4; i < iArray.Length; i += 5) // Шукаємо 5 елемент
+            {
+                int maxValue = int.MinValue; 
+
+                for (int j = i - 4; j < i; j++)
+                {
+                    if (iArray[j] > maxValue)
+                    {
+                        maxValue = iArray[j];
+                    }
+                }
+
+                iArray[i] = maxValue; 
+            }
+        }
+
+        public static void Print_Array(int[] Array)
+        {
+            foreach (int jA in Array)
+            {
+                Console.Write(jA + " ");
+            }
+            Console.WriteLine();
+        }
+       
+        public static int[] Claviatura(int Rosmir)
+        {
+            int j;
+            string strValue;
+            int[] iArray = new int[Rosmir];
+            Console.WriteLine($"Масив складається з {Rosmir} елементів");
+            for (j = 0; j < iArray.Length; j++)
+            {
+                Console.Write($"Введіть {j+1} елемент:");
+                strValue = Console.ReadLine();
+                iArray[j] = Convert.ToInt32(strValue);
+
+            }
+            return iArray;
+        }
+        public static int[] Random_number(int Rosmir)
+        {
+            int j;
+
+            int[] iArray = new int[Rosmir];
+            for (j = 0; j < iArray.Length; j++)
+            {
+                iArray[j] = rnd.Next(-101, 101); /* заповнюється числами від -100 до 100 */
+                
+            }
+            return iArray;
+        }
+       
+        public static int[] Text_file()
+        {
+            if (!File.Exists("dat.txt"))
+            {
+                Console.WriteLine("Файл не найден. Пожалуйста, создайте файл 'dat.txt' и добавьте числа.");
+                return new int[0];
+
+            }
+
+            int count = 0;
+            string strValue;
+            using (StreamReader sRead = new StreamReader("dat.txt"))
+            {
+                while (sRead.ReadLine() != null)
+                {
+                    count++;
+                }
+            }
+            int[] iArray = new int[count];
+
+            using (StreamReader sRead = new StreamReader("dat.txt"))
+            {
+
+                for (int j = 0; j < iArray.Length; j++)
+                {
+                    strValue = sRead.ReadLine();
+                    if (int.TryParse(strValue, out int number))
+                    {
+                        iArray[j] = number;
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Ошибка: '{strValue}' не число.Тому замінюємо його на 0");
+                        iArray[j] = 0;
+                    }
+                    
+                }
+                
+            }
+            return iArray;
+        }
+        public static double[][] Random_number_Dvoh(int Row,int Colm)
+        {
+            int j;
+            double[][] iArray = new double[Row][];
+            for (j = 0; j < Row; j++) 
+            {
+                iArray[j] = new double[Colm];
+                for (int i = 0; i < Colm; i++) 
+                {
+
+                    iArray[j][i] = rnd.Next(-101, 101); /* заповнюється числами від -100 до 100 */
+                
+                }
+
+            }
+            return iArray;
+        }
+        public static double[][] Claviatura_Dvoh(int Row, int Colm)
+        {
+            double[][] iArray = new double[Row][];
+            Console.WriteLine($"Масив складається з {Row}*{Colm} елементів");
+            for (int j = 0; j < Row; j++)
+            {
+                iArray[j] = new double[Colm];
+                for (int i = 0; i < Colm; i++) 
+                {
+                    Console.Write($"Введите элемент [{j + 1}, {i + 1}]: ");
+                    iArray[j][i] = Convert.ToDouble(Console.ReadLine()); 
+                }
+            }
+            return iArray;
+        }
+  
+        public static void Text_file_Dvoh()
+        {
+            if (!File.Exists("dat_2.txt"))
+            {
+                Console.WriteLine("Файл не найден. Пожалуйста, создайте файл 'dat_2.txt' и добавьте числа.");
+                return;
+
+            }
+            int rows = 0;
+            int cols = 0;
+            double[] x = null;
+            using (StreamReader reader = new StreamReader("dat_2.txt"))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    rows++;
+                    int currentCols = line.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Length;
+                    if (currentCols > cols)
+                    {
+                        cols = currentCols; // Запам'ятовуємо найбільшу довжину
+                    }
+                }
+            }
+
+            double[][] matrix = new double[rows][];
+
+            using (StreamReader reader = new StreamReader("dat_2.txt"))
+            {
+                for (int i = 0; i < rows; i++)
+                {
+                    string line = reader.ReadLine();
+                    string[] elements = line.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                    int count = elements.Length;
+                    matrix[i] = new double[count];
+                    for (int j = 0; j < count; j++)
+                    {
+                        if (int.TryParse(elements[j], out int number))
+                        {
+                            matrix[i][j] = number;
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Помилка: '{elements[j]}' не число. Замінюємо його на 1.");
+                            matrix[i][j] = 1;
+                        }
+                    }
+                }
+            }
+            Console.WriteLine("Масив: ");
+            Print_Matrix(matrix);
+            double[] A = FlattenJaggedArray(matrix);
+            x = Generasia_X(cols);
+          Nowei_masiv(x, A);
+        }
+        // 1 2 3
+        // 4 5 6 
+        // 7 8 9
+        //1 2 3 4 5 6 7 8 9 
+        //об'єднуємо всі підмасиви в один набір елементів
+        static double[] FlattenJaggedArray(double[][] matrix)
+        {
+            int totalLength = 0;
+            //Рахуємо кол елементів
+            for (int i = 0; i < matrix.Length; i++)
+            {
+                totalLength += matrix[i].Length;
+            }
+            double[] result = new double[totalLength];
+            int index = 0;
+            
+            for (int i = 0; i < matrix.Length; i++)
+            {
+                for (int j = 0; j < matrix[i].Length; j++)
+                {
+                    result[index++] = matrix[i][j];
+                }
+            }
+            return result;
+        }
+        public static double[] Generasia_X(int Colm)
+        {
+            double[] x = new double[Colm];
+            for (int j = 0; j < x.Length; j++){ 
+
+                 x[j] = rnd.Next(0,2); 
+            }
+            print_x(x);
+            return x;
+
+        }
+        static void print_x(double[] x)
+        {
+            Console.WriteLine("Масив (х)");
+            for (int i = 0; i < x.Length; i++)
+            {
+                Console.Write($"{x[i],8:F2}");
+            }
+            Console.WriteLine();
+        }
+        public static void Nowei_masiv(double[] x, double[] A)
+        {
+            double max = double.MinValue;
+            double min = double.MaxValue;
+            double dobavlen = 0;
+            int i = 0;
+            while (i < x.Length && i < A.Length)
+            {
+                    if (x[i] == 1)
+                    {
+                        A[i] = A[i] * (7 / 10);
+                    }
+                    else
+                    {
+                        A[i] = A[i] * (-1.4);
+                    }
+                    if (A[i] > 0 && A[i] > max)
+                    {
+                        max = A[i];
+                    }
+                    if(A[i] <0 && A[i] <min)
+                    {
+                        min = A[i];
+                    }
+                    i++;
+
+
+            }
+            Console.WriteLine("Масив,який помножили на 7/10 або -10/7");
+            Print_Masiv(A);
+            if (max == double.MinValue)
+            {
+                Console.WriteLine("Немає додатних чисел");
+                max = 0;
+            }
+            else
+            {
+                Console.WriteLine($"Найбільший серед додатних:{max,8:F2}");
+            }
+            if (min == double.MaxValue)
+            {
+                Console.WriteLine("Немає відємних чисел");
+                min = 0;
+            }
+            else
+            {
+                Console.WriteLine($"Найменший серед відємних:{min,8:F2}");
+
+            }
+            dobavlen = max + min;
+            for (int j = 0; j < A.Length; j++)
+            {
+
+                A[j] = A[j] + dobavlen;
+               
+
+            }
+            Console.WriteLine("");
+            Console.WriteLine($"Масив,який додали:{dobavlen,8:F2}");
+            Print_Masiv(A);
+           
+        }
+        static double[][] SortArray(double[][] A)
+        {
+            for (int i = 0; i < A.Length; i+=2) 
+            {
+
+                Array.Sort(A[i]);
+                Console.WriteLine(); 
+            }
+            return A;
+        }
+        public static void Print_Masiv(double[] iArray)
+        {
+            for (int j = 0; j < iArray.Length; j++)
+            {
+                Console.Write($"{iArray[j],8:F2}"); 
+                
+            }
+            Console.WriteLine(); 
+        }
+        static void Print_Matrix(double[][] iArray)
+        {
+            for (int j = 0; j < iArray.Length; j++)
+            {
+                for (int i = 0; i < iArray[j].Length; i++)
+                {
+                    Console.Write($"{iArray[j][i],8:F2}");
+                    
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+        public static void Task2() // (3+1+27)%30 = 1
+        {
+            int Row;
+            int Colm;
+            double[][] Array = null;
+            double[] x = null;
+            byte solution_dva = 0;
+            Console.WriteLine("Виберіть введення значень у масив написавши число 1,2,3 якщо бажаєте вийти напишіть любе інше число");
+            Console.WriteLine("1-Введення даних з клавіатури(через консоль)");
+            Console.WriteLine("2-Введення даних через файл");
+            Console.WriteLine("3-Рандомна генерація даних");
+            Console.Write("Введіть число:");
+            byte solution = Convert.ToByte(Console.ReadLine());
+            
+            switch (solution)
+            {
+                case 1:
+                    Console.WriteLine("Введіть тип багатовимірного масиву (1-прямокутний чи 2-ступінчатий)");
+                    Console.Write("Введіть число:");
+                    solution_dva = Convert.ToByte(Console.ReadLine());
+                    Console.Write("Введіть розмір масиву: ");
+                    Row = Convert.ToInt32(Console.ReadLine());
+                    if (solution_dva == 1)
+                    {
+                        Colm = Row;
+                    }
+                    else if (solution_dva == 2)
+                    {
+                        Console.Write("Введіть розмір стовбчиків");
+                       Colm = Convert.ToInt32(Console.ReadLine());
+                    }
+                    else
+                    {
+                        Console.Write("Ви ввели не те число вихід із програми...");
+                        return;
+                    }
+                    if(Row ==0 || Colm == 0)
+                    {
+                        Console.Write("Розмір масиву не може бути 0 ");
+                        return;
+                    }
+                    Array = Claviatura_Dvoh(Row, Colm);
+                    x= Generasia_X(Row);
+                    break;
+                case 2:
+                    Text_file_Dvoh();
+                    break;
+                case 3:
+                    Console.WriteLine("Введіть тип багатовимірного масиву (1-прямокутний чи 2-ступінчатий)");
+                    Console.Write("Введіть число:");
+                    solution_dva = Convert.ToByte(Console.ReadLine());
+                    Console.Write("Введіть розмір масиву: ");
+                    Row = Convert.ToInt32(Console.ReadLine());
+                    if (solution_dva == 1)
+                    {
+                        Colm = Row;
+                    }
+                    else if (solution_dva == 2)
+                    {
+                        Console.Write("Введіть розмір стовбчиків");
+                        Colm = Convert.ToInt32(Console.ReadLine());
+                    }
+                    else
+                    {
+                        Console.Write("Ви ввели не те число вихід із програми...");
+                        return;
+                    }
+                    if (Row == 0 || Colm == 0)
+                    {
+                        Console.Write("Розмір масиву не може бути 0 ");
+                        return;
+                    }
+                    Array = Random_number_Dvoh(Row, Colm);
+                    x=Generasia_X(Row);
+                    break;
+                default:
+                    Console.WriteLine("Вихід із програми...");
+                    return;
+            }
+            double[] A = FlattenJaggedArray(Array);
+            Nowei_masiv(x, A);
+            Array = SortArray(Array);
+            Console.WriteLine("Масив який відсортували у порядок зростання: ");
+            Print_Matrix(Array);
+
+        }
+        public static void Task1()
+        {
+            int Rosmir;
+            int[] Array = null;
+            Console.WriteLine("Виберіть введення значень у масив написавши число 1,2,3 якщо бажаєте вийти напишіть любе інше число");
+            Console.WriteLine("1-Введення даних з клавіатури(через консоль)");
+            Console.WriteLine("2-Введення даних через файл");
+            Console.WriteLine("3-Рандомна генерація даних");
+            Console.WriteLine("Введіть число:");
+            byte solution = Convert.ToByte(Console.ReadLine());
+            switch (solution)
+            {
+                case 1:
+                    Console.Write("Введіть розмір масиву: ");
+                    Rosmir = Convert.ToInt32(Console.ReadLine());
+                    if (Rosmir == 0)
+                    {
+                        Console.Write("Розмір масиву не може бути 0 ");
+                        return;
+                    }
+                    Array = Claviatura(Rosmir);
+                    break;
+                case 2:
+                    Array = Text_file();
+                    break;
+                case 3:
+                    Console.Write("Введіть розмір масиву: ");
+                    Rosmir = Convert.ToInt32(Console.ReadLine());
+                    if (Rosmir == 0)
+                    {
+                        Console.Write("Розмір масиву не може бути 0 ");
+                        return;
+                    }
+                    Array = Random_number(Rosmir);
+                    break;
+                default:
+                    Console.WriteLine("Вихід із програми...");
+                    return;
+            }
+            Videm_element(Array);
+            var element = Videm_element(Array);
+            Console.WriteLine($"Сума від'ємних ={element.sum_otr}");
+            Console.WriteLine($"Кількість від'ємних ={element.num_otr}");
+            Console.WriteLine("Старий масив: ");
+            Print_Array(Array);
+            Console.WriteLine("Новий масив: ");
+            Change_five(Array);
+            Print_Array(Array);
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Виберіть завдання від 1 до 2");
+            Console.Write("Напишіть число:");
+            byte solution = Convert.ToByte(Console.ReadLine());
+            switch (solution)
+            {
+                case 1:
+                    {
+                        Task1();
+                        break;
+                    }
+                case 2:
+                    {
+                        Task2();
+
+                        break;
+                    }
+                default:
+                    Console.WriteLine("Error");
+                    return;
+            }
+        }
+    }
+}
---
+++ Лабораторні курс 1/Laboratorka_4/Program.cs
@@ -0,0 +1,281 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Laboratorka_4
+{
+    internal class Program
+    {
+        public static string CheckingForANumber()
+        {
+            while (true)
+            {
+                string arr;
+                arr = Console.ReadLine();
+
+                if (int.TryParse(arr, out int number))
+                {
+                    Console.WriteLine("Введене значення є числом.");
+                    Console.WriteLine("Введіть відповідь повторно");
+                }
+                else
+                {
+                    return arr;
+                }
+            }
+        }
+        static void Task1_1()
+        {
+            Console.Write("Напишіть вираз: ");
+            string user_answer = Console.ReadLine();
+            if (Regex.IsMatch(user_answer, @"^[a-v]{22}18340$"))
+            {
+                Console.WriteLine($"правильний вираз:{user_answer}");
+            }
+            else
+            {
+                Console.WriteLine($"невірний вираз:{user_answer}");
+            }
+            
+
+        }
+        static void Task5()
+        {
+            string anwser;
+            string user_answer = "";
+            Console.WriteLine("Код визначає, чи є заданий рядок  шістнадцятковим ідентифікатором кольору HTML");
+            Console.Write("Чи потрібна вам допомога введіть так або нет: ");
+            string solution = CheckingForANumber();
+            if (solution.ToLower() == "так") // добавил от себя
+            {
+                Console.WriteLine("Приклад #FFFF00");
+                Console.Write("Введіть шістнадцятковим ідентифікатором кольору:#");
+                anwser = Console.ReadLine();
+                if (anwser.Length > 6)
+                {
+                    anwser = anwser.Substring(0, 6);
+                }
+                user_answer = $"#{anwser}".ToUpper();
+              
+
+            }
+            else if(solution.ToLower() == "нет")
+            {
+                Console.Write("Введіть шістнадцятковим ідентифікатором кольору: ");
+                user_answer = Console.ReadLine();
+                if (user_answer.Length > 7)
+                {
+                    user_answer = user_answer.Substring(0, 7);
+                }
+               
+            }
+            else
+            {
+                Console.WriteLine("Вихід із програми...");
+                return;
+            }
+            // проверяет на перший символ хештег(щоб був одни раз) і 6 символів після нього
+            if (Regex.IsMatch(user_answer, @"^#([A-Fa-f0-9]{6})$"))
+            {
+                Console.WriteLine($"Вираз правильний: {user_answer}");
+            }
+            else
+            {
+                Console.WriteLine($"Неправильний вираз: {user_answer}");
+            }
+            Console.WriteLine("Натисніть Enter для продовження...");
+            Console.ReadKey();
+        }
+
+        static string[] Ruchne()
+        {
+            try
+            {
+                Console.WriteLine("Напишіть слова,розділяйте кожне слово пробілом: ");
+                string sent = Console.ReadLine();
+                string[] words;
+                words = sent.Split(' ');
+                print(words);
+                return words;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error reading file: {ex.Message}");
+                return new string[0];
+            }
+        }
+        static string[] Two_in_one(string[] words)
+        {
+            string[] result = new string[(words.Length+1) / 2];
+            for (int i = 1,j=0; j < result.Length; i += 2,j++)
+            {
+                if (i < words.Length)
+                {
+                    result[j] += words[i-1] + words[i] + " ";
+                }
+                else
+                {
+                    result[j] += words[i-1];
+                }
+            }
+            return result;
+        }
+        static void Write_text(string[] result,string txt)
+        {
+            using (StreamWriter f2 = new StreamWriter(txt))
+            {
+                foreach (string line in result)
+                {
+                    f2.Write(line);
+                }
+            }
+        }
+        static void print(string[] words)
+        {
+            foreach (string jA in words)
+            {
+                Console.Write(jA + " ");
+            }
+            Console.WriteLine(" ");
+            Console.WriteLine("Кількість слів у рядку: " + words.Length);
+            Console.WriteLine("Натисніть Enter для продовження...");
+            Console.ReadKey();
+
+        }
+        static string[] text()
+        {
+            Console.Write("Введіть ім'я файлу в якому дані: ");
+            string txt = Console.ReadLine();
+            if (!File.Exists($"{txt}.txt"))
+            {
+                Console.WriteLine($"Файл не найден. Пожалуйста, создайте файл '{txt}.txt' и добавьте числа.");
+                return new string[0];
+
+            }
+
+            string strValue;
+            string[] words ;
+
+            using (StreamReader sRead = new StreamReader($"{txt}.txt"))
+            {
+
+                    strValue = sRead.ReadToEnd();
+                    words = strValue.Split(' ');
+
+
+
+                print(words);
+            }
+            return words;
+        }
+        static void Task2()
+        {
+            string[] words = null;
+            string[] result = null;
+            Console.Write("Введіть ім'я файлу для збереження інформації: ");
+            string txt = Console.ReadLine();
+            if (!File.Exists($"{txt}.txt"))
+            {
+                Console.WriteLine($"Файл не найден. Пожалуйста, создайте файл '{txt}.txt' и добавьте числа.");
+                return;
+
+            }
+            Console.WriteLine("Цей код об'єднує у одне слово кожні два слова рядка, що поряд розташовані");
+            Console.Write("Виберіть тип введення даних,напишіть ручне або файл: ");
+            string solution = CheckingForANumber();
+            Console.WriteLine("Натисніть Enter для продовження...");
+            Console.ReadKey();
+            Console.Clear();
+            if (solution.ToLower() == "ручне") 
+            {
+                try
+                {
+                    words = Ruchne();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error reading file: {ex.Message}");
+                    return;
+                }
+            }
+            else if (solution.ToLower() == "файл")
+            {
+            
+                try
+                {
+                    words = text();
+
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error reading file: {ex.Message}");
+                    return;
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("Вихід із програми...");
+                return;
+            }
+            result = Two_in_one(words);
+            print(result);
+            Write_text(result, $"{txt}.txt");
+        }
+       
+        static void Task1()
+        { //1 and 5
+            Console.WriteLine("Це завдання складається з падзавдання 1 та 5");
+            Console.WriteLine("Виберіть завдання 1 або 5,якщо бажаєте вийти напишіть любе друге число");
+            Console.Write("Напишіть число:");
+            byte solution = Convert.ToByte(Console.ReadLine());
+            Console.WriteLine("Натисніть Enter для продовження...");
+            Console.ReadKey();
+            Console.Clear();
+            switch (solution)
+            {
+                case 1:
+                    {
+                        Task1_1();
+                        break;
+                    }
+                case 5:
+                    {
+                        Task5();
+                        break;
+                    }
+                default:
+                    {
+                        Console.WriteLine("Вихід із програми");
+                        return;
+                    }
+
+
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Виберіть завдання від 1 до 2");
+            Console.Write("Напишіть число:");
+            byte solution = Convert.ToByte(Console.ReadLine());
+            switch (solution)
+            {
+                case 1:
+                    {
+                        Task1();
+                        break;
+                    }
+                case 2:
+                    {
+                        Task2();
+                        break;
+                    }
+                default:
+                    {
+                        Console.WriteLine("Error");
+                        return;
+                    }
+
+
+            }
+        }
+    }
+}
